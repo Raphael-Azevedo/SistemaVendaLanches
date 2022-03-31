f825409dacc73fbd71bf99e975c3219630ce04aa
perkles <rfcompanhia@hotmail.com>
2022-03-31 16:59:23 -0300

    Criação do projeto MVC

---
+++ MVC2022/Models/Lanche.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC2022.Models
+{
+    [Table("Lanches")]
+    public class Lanche
+    {
+        [Key]
+        public int LancheId { get; set; }
+
+        [Required(ErrorMessage ="O nome do lanche deve ser informado")]
+        [Display(Name ="Nome do Lanche")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} caracteres")]
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
